b1fbb19ff9083e81261806d228f1c8635fa816f4
AliceYangAC <yang0447@algonquinlive.com>
2025-12-02 18:07:13 -0500

    Add shipping service endpoint and update fetch URL in App.vue

--- src/App.vue
+++ src/App.vue
@@ -93,7 +93,7 @@ export default {
         status: 2
       };
 
-  await fetch(`${shippingServiceUrl}ship`, {
+  await fetch(`${shippingServiceUrl}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(payload)
